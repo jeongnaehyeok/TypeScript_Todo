09661516cf2b9f493e6f9a2f663f90b7d19e863b
jeongnaehyeok <cnh0301@gmail.com>
2021-01-02 23:22:42 +0900

    [ Feature ] Delete 개발

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import Input from './components/Input';
 import Items from './components/Items';
+import './App.css';
 
 function App() {
   const [todoList, setTodoList] = useState<string[]>([]);
   const onAddTodo = (_value: string) => {
     setTodoList([...todoList, _value]);
   };
+  const onDeleteTodo = (_index: string) => {
+    setTodoList([...todoList.filter(index => index !== _index)]);
+  };
   return (
     <div className="App">
       <Input onKeyDown={onAddTodo} />
-      <Items itemList={todoList} />
+      <Items itemList={todoList} onDeleteItem={onDeleteTodo} />
     </div>
   );
 }
--- src/components/Items.tsx
+++ src/components/Items.tsx
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Items.css';
 
 export interface ItemsProps {
   itemList: string[];
+  onDeleteItem: (index: string) => void;
 }
 
-function Items({ itemList }: ItemsProps) {
+function Items({ itemList, onDeleteItem }: ItemsProps) {
+  const [items, setItems] = useState<string[]>([]);
+  const onItemDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
+    onDeleteItem(e.currentTarget.value);
+  };
+  useEffect(() => {
+    setItems(itemList);
+  }, [itemList]);
   return (
-    <div>
-      {itemList.map((item: string, index: number) => {
-        return <p key={`${index.toString()}_${item}`}>{item}</p>;
+    <ul className="items--container">
+      {items.map((item: string, index: number) => {
+        return (
+          <li className="items--item" key={`${index.toString()}_${item}`}>
+            <p>
+              {index}
+              번째
+            </p>
+            <p>{item}</p>
+            <button
+              className="item--delete"
+              type="button"
+              value={index}
+              onClick={onItemDelete}
+            >
+              X
+            </button>
+          </li>
+        );
       })}
-    </div>
+    </ul>
   );
 }
 Items.defaultProps = {
   itemList: [],
+  onDeleteItem: () => {},
 } as Partial<ItemsProps>;
 
 export default Items;
